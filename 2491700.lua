eab345d86ecd611eed8fc07dbada45298930e483
βασιλιάς <gabriellopeskingle@gmail.com>
2025-05-21 15:14:32 +0000

    2491700

---
+++ 2491700.lua
@@ -0,0 +1,8 @@
+addappid(2491700)
+addappid(228990)
+setManifestid(228990,"5087715316087945828")
+addappid(229033)
+addappid(2491701,0,"38d0d478fd0fae3bfc5ae6cb6a46a7bd2b7e3ad3dc942d27a78f36cf97cb5e55")
+setManifestid(2491701,"1877805277876846425")
+addappid(2491702)
+addappid(2491703)
